828af7e4d455534502ebc05b28fbc3ff343256f2
Adheeb <za22adj@herts.ac.uk>
2023-05-08 01:59:26 +0100

    Update Infographics Code.py

--- Infographics Code.py
+++ Infographics Code.py
@@ -25,6 +25,7 @@ electricity_access, electricity_access_t = read_data('Access to electricity (% o
 co2_emission, co2_emission_t = read_data('CO2 emissions (metric tons per capita).csv')
 electricity_hydro, electricity_hydro_t = read_data('electricity_production_hydro.csv')
 electricity_hydro_t = electricity_hydro_t[:17]
+electricity_hydro_t = electricity_hydro_t.apply(pd.to_numeric)
 electricity_nonrenewable, electricity_nonrenewable_t = read_data('electricity_production_nonrenewable.csv')
 electricity_nonrenewable_t = electricity_nonrenewable_t[:17]
 electricity_nuclear, electricity_nuclear_t = read_data('electricity_production_nuclear.csv')
@@ -38,8 +39,17 @@ for i in range(len(Countries_list)):
     ax[0, 0].tick_params(axis='x', rotation=90)
     '''
 plt.figure()
+for i in range(len(Countries_list)):
+    plt.plot(electricity_hydro_t.index, electricity_hydro_t[Countries_list[i]])
+             
+"""
+plt.plot(electricity_hydro_t.index, electricity_hydro_t["United Kingdom"],
+         electricity_hydro_t["China"],electricity_hydro_t["United States"], 
+         electricity_hydro_t["India"], electricity_hydro_t["Indonesia"], 
+         electricity_hydro_t["Thailand"], electricity_hydro_t["Bangladesh"])
+"""
+plt.legend(Countries_list)
 
-plt.plot(electricity_hydro_t.index, electricity_hydro_t["India"], electricity_nonrenewable_t["India"])
 #ax.yaxis.set_ticks(np.arange(0, 2047, 32))
 plt.xticks(rotation=90)
 plt.show()
